7cad274a339cd1be9c8d1f237ca1d220ef93b529
Tony Thompson <penguinprofession@icloud.com>
2018-07-03 12:47:55 -0400

    refactored with keyword arugments

--- blockchain.py
+++ blockchain.py
@@ -8,13 +8,13 @@ def get_last_block_value():
 
 
 # append latest value to blockchain
-def add_value(transaction_amount, last_transaction):
+def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
 # test with values
-add_value(2, [1])
-add_value(.6, get_last_block_value())
-add_value(20.2, get_last_block_value())
+add_value(transaction_amount=2)
+add_value(last_transaction=get_last_block_value(), transaction_amount=.6)
+add_value(transaction_amount=20.2, last_transaction=get_last_block_value())
 
 print(blockchain)
