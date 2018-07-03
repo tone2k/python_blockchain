fd9ba29089f2277cffa1c9cec87e10e76b7c181f
Tony Thompson <penguinprofession@icloud.com>
2018-07-03 12:54:03 -0400

    added in input for value

--- blockchain.py
+++ blockchain.py
@@ -12,7 +12,9 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-# test with values
+# test with values, prompt for input
+tx_amount=float(input('Your transaction amount please: '))
+add_value(tx_amount)
 add_value(transaction_amount=2)
 add_value(last_transaction=get_last_block_value(), transaction_amount=.6)
 add_value(transaction_amount=20.2, last_transaction=get_last_block_value())
