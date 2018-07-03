4e9c9b03051dd4b76a82b013aa357d3a3e2429d7
Tony Thompson <penguinprofession@icloud.com>
2018-07-03 12:32:02 -0400

    created transaction amount value

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,9 @@
-blockchain = []
+blockchain = [[1]]
 
-def add_value(): 
-    blockchain.append(5.3)
+def add_value(transaction_amount): 
+    blockchain.append([blockchain[-1], transaction_amount])
     print(blockchain)
 
-add_value()
+add_value(2)
+add_value(.6)
+add_value(20.2)
