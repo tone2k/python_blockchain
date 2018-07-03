608e7bdc8a8a148e400f0aa211681f1d66a486c1
Tony Thompson <penguinprofession@icloud.com>
2018-07-03 12:34:55 -0400

    refactored for readablity and adjusted arguements to function

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,16 @@
 blockchain = [[1]]
 
+# extract last value from blockchain
+def get_last_block_value():
+    return blockchain[-1]
+
+# append latest value to blockchain
 def add_value(transaction_amount): 
-    blockchain.append([blockchain[-1], transaction_amount])
+    blockchain.append([get_last_block_value(), transaction_amount])
     print(blockchain)
 
+
+# test with values
 add_value(2)
 add_value(.6)
 add_value(20.2)
