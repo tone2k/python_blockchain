083f1d7de0b2875dad0b83e3f0bbdad38b07cc30
Tony Thompson <penguinprofession@icloud.com>
2018-07-03 12:58:43 -0400

    refactored into function get user input

--- blockchain.py
+++ blockchain.py
@@ -12,11 +12,20 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
+def get_user_input():
+    return float(input('Your transaction amount please: '))
+
+
 # test with values, prompt for input
-tx_amount=float(input('Your transaction amount please: '))
+tx_amount = get_user_input()
 add_value(tx_amount)
-add_value(transaction_amount=2)
-add_value(last_transaction=get_last_block_value(), transaction_amount=.6)
-add_value(transaction_amount=20.2, last_transaction=get_last_block_value())
+
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_block_value(),
+          transaction_amount=tx_amount)
+
+tx_amount = get_user_input()
+add_value(transaction_amount=tx_amount,
+          last_transaction=get_last_block_value())
 
 print(blockchain)
