497d97992d028560660e9f69e8f4cefb1e37527f
Tony Thompson <penguinprofession@icloud.com>
2018-07-19 12:32:28 -0400

    added in print option for user

--- blockchain.py
+++ blockchain.py
@@ -43,6 +43,7 @@ while True:
     print('Please Choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
