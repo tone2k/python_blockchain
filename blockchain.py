f292c67c771e358fbc18ad42afb3235196a19609
Tony Thompson <penguinprofession@icloud.com>
2018-07-19 13:41:20 -0400

    created if statement in get block value function that checks if value is less than 1, if so, return none

--- blockchain.py
+++ blockchain.py
@@ -4,17 +4,21 @@ blockchain = []
 # extract last value from blockchain
 def get_last_block_value():
     """Returns the last value of the Blockchain"""
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
 # append latest value to blockchain
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """Append a new value and the last value to the latest block
     
     Arguments:
         :transaction_amount: The amount that should be added.
         :last_transaction: The last blockchain transaction value. (default [1])
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -34,11 +38,6 @@ def print_blockchain_elements():
         print('Outputting Block')
         print(block)
 
-
-# test with values, prompt for input
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
 while True:
     print('Please Choose')
     print('1: Add a new transaction value')
@@ -47,7 +46,7 @@ while True:
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_block_value())
+        add_transaction(tx_amount, get_last_block_value())
     elif user_choice == '2':
         print_blockchain_elements()
     elif user_choice == 'q':
